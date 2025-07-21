200f7005ba79f6408a5b143c77cfe2a771c9428b
dio067 <yousefnas06@gmail.com>
2025-07-21 09:21:21 +0400

    fix: edit names of templates

--- views/authentication/signin.js
+++ views/authentication/signin.js
@@ -1,29 +1,34 @@
 import layout from "../layouts/authentication.js";
 
-export default (errors) => {
+export default ({ errors }) => {
 	return layout(`
-        		<div class="wrapper">
-			<h1>Sign Up</h1>
-			<div class="input-box">
-				<input type="text" required />
-				<label>Email</label>
-				<i class="bx bxs-user"></i>
-			</div>
-			<div class="input-box">
-				<input type="password" required />
-				<label>Password</label>
-				<i class="bx bxs-lock-alt"></i>
-			</div>
-			<div class="input-box">
-				<input type="password" required />
-				<label>Password Confirmation</label>
-			</div>
+		<div class="wrapper">
+			<h1>Login</h1>
+			<form action="">
+				<div class="input-box">
+					<input type="text" value="" required />
+					<label for="">Email</label>
+					<i class="bx bxs-user"></i>
+				</div>
+				<div class="input-box">
+					<input type="password" required />
+					<label for="">Password</label>
+					<i class="bx bxs-lock-alt"></i>
+				</div>
+				<div class="remember-forgot">
+					<label>
+						<input type="checkbox" />
+						Remember me
+					</label>
+					<a href="#">Forgot Password?</a>
+				</div>
 
-			<button class="btn" type="submit">Register</button>
+				<button type="submit" class="btn">Login</button>
 
-			<div class="login-link">
-				<p>Already have an account?<a href="/signin"> Login</a></p>
-			</div>
+				<div class="register-link">
+					<p>Don't have an account? <a href="/signup">Register</a></p>
+				</div>
+			</form>
 		</div>
         `);
 };
