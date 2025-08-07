cf26d48b90bd8ba07396208b9e6a53c6d65be6d0
dio067 <yousefnas06@gmail.com>
2025-08-07 14:59:28 +0400

    feat: add hidden class to the sidebar

--- views/pages/home.js
+++ views/pages/home.js
@@ -1,26 +1,27 @@
 import layout from '../layouts/pages.js';
 export default ({ errors }) => {
-  return layout(`<div class="grid-container">
+  return layout(`<form method:"post">
+	<div class="grid-container">
 			<header class="header">
-				<div class="menu-icon" onclick="openSidebar()">
-					<span class="material-symbols-outlined"> menu </span>
-				</div>
 				<div class="header-left">
-					<span class="material-symbols-outlined"> brightness_empty </span>
+					<div class="menu-icon">
+						<span class="material-symbols-outlined"> menu </span>
+					</div>
 				</div>
 				<div class="header-right">
+					<span class="material-symbols-outlined"> brightness_empty </span>
 					<span class="material-symbols-outlined"> notifications </span>
 					<span class="material-symbols-outlined"> mail </span>
 					<span class="material-symbols-outlined"> account_circle </span>
 				</div>
 			</header>
-			<aside class="sidebar" id="sidebar">
+			<aside class="sidebar sidebar-hidden" id="sidebar">
 				<div class="sidebar-title">
 					<div class="sidebar-signture">
 						<span class="material-symbols-outlined"> chess_knight </span>Menu
-						<span class="material-symbols-outlined" onclick="closeSidebar()">
-							close
-						</span>
+						<span class="material-symbols-outlined" onclick="closeSidebar()"
+							>close</span
+						>
 					</div>
 				</div>
 				<ul class="sidebar-list">
@@ -29,19 +30,17 @@ export default ({ errors }) => {
 							<span class="material-symbols-outlined"> chess_pawn </span>PLAY
 						</li>
 					</a>
-
 					<a href="/analyze">
 						<li class="sidebar-list-item">
 							<span class="material-symbols-outlined"> troubleshoot </span
 							>ANALYZE
 						</li>
 					</a>
-
 					<a href="/database">
 						<li class="sidebar-list-item">
 							<span class="material-symbols-outlined"> database </span>DATABASE
-						</li></a
-					>
+						</li>
+					</a>
 				</ul>
 			</aside>
 			<main class="main-container">
@@ -63,5 +62,6 @@ export default ({ errors }) => {
 					</div>
 				</div>
 			</main>
-		</div>`);
+		</div>
+		</form>`);
 };
